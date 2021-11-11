84d55dbfe8dd057f18d2a9bb8a6ab1b71241e74b
earth13wells <Gabrielw.presley@protonmail.com>
2021-11-10 19:50:35 -0800

    Initial commit

---
+++ spec/compiler-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import CompilerView from '../lib/compiler-view';
+
+describe('CompilerView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
